410ece90451147db0514a67cc5eac518bf66bbf0
riley soloner <rileysoloner@gmail.com>
2022-11-14 19:09:41 -0500

    commit tree 346bf611cb4f8374878077d08faf31cd217daccc

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let i = array[0];
+  let j = i + 1;
+  if (i + j == target) {
+    return true
+  } else {
+    i++
+  }
 }
 
 /* 
@@ -32,3 +38,32 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+// 1. Rewrite the Problem in Your Own Words:
+
+// write an algorithm that takes in two arguments: 
+// an array of numbers
+// a target integer, aka a number that is the desired sum of any two numbers in the array.
+// the algorithm returns true if the target integer is achievable by adding any two numbers of the array.
+
+// 2. Write Your Own Test Cases
+
+// hasTargetSum([1, 2, 5, 10, 11], 7)
+// returns true
+
+// hasTargetSum([1, 2, 5, 10, 11], 90)
+// returns false
+
+// 3. Pseudocode
+
+// ok so inside the function
+// define the first indexed number in an array, AKA array[0] lets call it i.
+// define a second number, j. j is i + 1
+// if i + j = target, return true
+// else iterate somehow. so like array[1] + array[2]
+// i don't fucking know yet.
+
+
+// Code
+// Make It Clean and Readable
+// Optimize
